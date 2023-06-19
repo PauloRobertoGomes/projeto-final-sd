cda7718effbfc69cb4f1c2fceb5ac123d7ee6ff1
Paulo Roberto <paulo.roberto@lapisco.ifce.edu.br>
2023-06-19 01:32:18 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# projeto-final-sd
+# RESTful com Node.js, Express e MongoDB
+
+### Pré-requisitos
+
+- **Node.js** versão 8 ou superior;
+
+### Instalação
+
+1. Faça o clone do repositório e no terminal navegue até a pasta;
+2. Instale as dependências do projeto com `npm install`;
+3. Rode o servidor de desenvolvimento com `npm run start:dev`.
+4. O _endpoint_ do serviço estará disponível em _http://localhost:8080/api/v1_.
+
+### License
+
+[MIT License Copyright (c) 2023 Paulo Roberto](https://github.com/PauloRobertoGomes)
+
+> Permission is hereby granted, free
+> of charge, to any person obtaining a copy of this software and associated
+> documentation files (the "Software"), to deal in the Software without
+> restriction, including without limitation the rights to use, copy, modify, merge,
+> publish, distribute, sublicense, and/or sell copies of the Software, and to
+> permit persons to whom the Software is furnished to do so, subject to the
+> following conditions:
+
+> The above copyright notice and this permission notice
+> (including the next paragraph) shall be included in all copies or substantial
+> portions of the Software.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+> ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+> MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+> EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+> OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+> FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+> THE SOFTWARE.
---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const uri = "mongodb://localhost:27017/sd";
+const port = 8080;
+const app = express();
+const routes = express.Router();
+
+//Configuração para tratamento dos dados vindo via api
+app.use(express.json());
+
+//Banco de dados
+
+//Criação do esquema/estrutura do usuário
+const userSchema = new mongoose.Schema({
+  nome: {
+    required: true,
+    type: String,
+  },
+  ativo: {
+    required: true,
+    type: Boolean,
+  },
+  id: {
+    required: true,
+    type: Number,
+  },
+});
+
+//Criação do model do usuário baseado no esquema montado acima
+const userModel = mongoose.model("Data", userSchema);
+
+//Conexão com o bd
+mongoose.connect(uri);
+const database = mongoose.connection;
+
+//Verifica se tiver erros
+database.on("error", (error) => {
+  console.log(error);
+});
+
+//Indica quando é conectada
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+//API
+
+//Listar todos os usuários
+routes.get("/", async (req, res) => {
+  try {
+    const data = await userModel.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Pega usuário pelo id
+routes.get("/:id", async (req, res) => {
+  try {
+    const data = await userModel.findOne({ id: req.params.id });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Criar um novo usuário
+routes.post("/", async (req, res) => {
+  console.log(req.body);
+  const data = new userModel({
+    nome: req.body.nome,
+    ativo: req.body.ativo,
+    id: req.body.id,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Atualizar informações pelo id
+routes.put("/:id", async (req, res) => {
+  try {
+    await userModel.findOneAndUpdate({ id: req.params.id }, req.body);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Deletar usuário pelo id
+routes.delete("/:id", async (req, res) => {
+  try {
+    await userModel.findOneAndDelete(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Concatenação das rotas com a url base
+app.use("/api/v1/user/", routes);
+
+//Inicialização do server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
